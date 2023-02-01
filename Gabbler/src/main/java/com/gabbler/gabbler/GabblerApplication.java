b1cb7ae1585e8ef6879dc0e657dcc27fbb3dcd81
Sinsay63 <yanis.houdier@gmail.com>
2023-02-01 01:16:56 +0100

    création project spring + ajout mcd

---
+++ Gabbler/src/main/java/com/gabbler/gabbler/GabblerApplication.java
@@ -0,0 +1,13 @@
+package com.gabbler.gabbler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabblerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GabblerApplication.class, args);
+    }
+
+}
---
+++ Gabbler/src/test/java/com/gabbler/gabbler/GabblerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabbler.gabbler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GabblerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
