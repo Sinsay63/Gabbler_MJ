ed1c5428fbd9a40f2fde5c3c3895fddb2fbd87a2
loristrr <loristerry63430@gmail.com>
2023-03-09 16:33:13 +0100

    identification

--- gabbler-cli/src/app/app.module.ts
+++ gabbler-cli/src/app/app.module.ts
@@ -6,20 +6,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './components/accueil/accueil.component';
 import { ConnexionComponent } from './components/connexion/connexion.component';
-import { EnregistrerComponent } from './components/enregistrer/enregistrer.component';
 import { SideMenuComponent } from './components/accueil/side-menu/side-menu.component';
 import { MainContentComponent } from './components/accueil/main-content/main-content.component';
 import { SuggestionComponent } from './components/accueil/suggestion/suggestion.component';
+import { IdentificationComponent } from './components/identification/identification.component';
+import { LoginComponent } from './components/identification/login/login.component';
+import { RegisterComponent } from './components/identification/register/register.component';
+import { SliderComponent } from './components/identification/slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AccueilComponent,
     ConnexionComponent,
-    EnregistrerComponent,
     SideMenuComponent,
     MainContentComponent,
-    SuggestionComponent
+    SuggestionComponent,
+    IdentificationComponent,
+    LoginComponent,
+    RegisterComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
--- gabbler-cli/src/app/components/identification/login/login.component.spec.ts
+++ gabbler-cli/src/app/components/identification/login/login.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { EnregistrerComponent } from './enregistrer.component';
+import { LoginComponent } from './login.component';
 
-describe('EnregistrerComponent', () => {
-  let component: EnregistrerComponent;
-  let fixture: ComponentFixture<EnregistrerComponent>;
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EnregistrerComponent ]
+      declarations: [ LoginComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(EnregistrerComponent);
+    fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
